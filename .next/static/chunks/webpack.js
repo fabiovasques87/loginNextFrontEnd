fd5fb72b9032e4b70a82ab8edd93b2fc75b86945
fabiovasques87 <fabioportovasques@yahoo.com.br>
2023-11-24 23:19:35 -0500

    add background em torno de formulario, dentro da opnão de cadastrar e ajustado a responsividade

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ada5a063c0f31a35"; }
+/******/ 		__webpack_require__.h = function() { return "32ca4c8d4a2356cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
