74e9af2f989ca0573a7f47ba65f5f719c1e7198f
fabiovasques87 <fabioportovasques@yahoo.com.br>
2023-11-27 13:15:24 -0300

    updates do next.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0968dbdf0eadf01b"; }
+/******/ 		__webpack_require__.h = function() { return "8ccf48cc377ae1b5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
